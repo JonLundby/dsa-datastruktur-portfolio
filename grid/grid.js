058e33303f74d1cb136ec2477ad19c31802a9430
Jon <lundbyjon@hotmail.com>
2024-09-24 21:43:34 +0200

    nabo funktioner tiløjet

--- grid/grid.js
+++ grid/grid.js
@@ -1,5 +1,3 @@
-
-
 export default class Grid {
     constructor(rows, cols, cellValue) {
         this.rows = rows;
@@ -15,8 +13,7 @@ export default class Grid {
             for (let j = 0; j < cols; j++) {
                 if (cellValue || cellValue === null) {
                     grid[i].push(cellValue);
-                }
-                else {
+                } else {
                     grid[i].push(i * cols + j);
                 }
             }
@@ -39,7 +36,7 @@ export default class Grid {
     _isValidPosition(arg1, arg2) {
         // hvis arg1 er et objekt, så bruges parsePosition til at "udpakke" objektet til row og col
         const { row, col } = this._parsePosition(arg1, arg2);
-        
+
         // returner true hvis row og col er inden for gridets grænser ellers returneres false
         return row >= 0 && row < this.rows && col >= 0 && col < this.cols;
     }
@@ -50,23 +47,23 @@ export default class Grid {
     }
 
     set(arg1, arg2, arg3) {
-        const { row, col } = this._parsePosition(arg1, arg2)
+        const { row, col } = this._parsePosition(arg1, arg2);
 
         // hvis positionen er gyldig...
         if (this._isValidPosition(row, col)) {
             // ...og arg1 er typen af objekt så indsættes arg2 som værdi og er arg1 IKKE et objekt så indsættes arg3 som værdi
-            if (typeof arg1 === 'object') {
-                this.grid[row][col] = arg2
+            if (typeof arg1 === "object") {
+                this.grid[row][col] = arg2;
             } else {
-                this.grid[row][col] = arg3
+                this.grid[row][col] = arg3;
             }
         } else {
-            return undefined
+            return undefined;
         }
     }
 
     get(arg1, arg2) {
-        const {row, col } = this._parsePosition(arg1, arg2)
+        const { row, col } = this._parsePosition(arg1, arg2);
         if (this._isValidPosition(row, col)) {
             return this.grid[row][col];
         } else {
@@ -75,7 +72,7 @@ export default class Grid {
     }
 
     indexFor(arg1, arg2) {
-        if(this._isValidPosition(arg1, arg2)) {
+        if (this._isValidPosition(arg1, arg2)) {
             const { row, col } = this._parsePosition(arg1, arg2);
             return row * this.cols + col;
         } else {
@@ -84,13 +81,112 @@ export default class Grid {
     }
 
     rowColFor(index) {
-        let i = 0
-
+        // tjek om index er gyldig og indenfor grid
         if (this._isValidIndex(index)) {
-            const row = Math.floor(index / this.cols) // 1
-            const col = index % this.cols
+            // hvis index er gyldig findes den række som index befinder sig i ved at dividere index-tallet med antallet af kolonner
+            const row = Math.floor(index / this.cols); // 1
+            // og kolonnen som index befinder sig i findes ved at tage det overskydende tal(modolus) ved division af index med antal kolonner
+            const col = index % this.cols;
 
-            return {row, col}
+            return { row, col };
+        } else {
+            return undefined;
+        }
+    }
+
+    neighbours(arg1, arg2) {
+        const { row, col } = this._parsePosition(arg1, arg2);
+        let neighboursActual = [];
+
+        if (this._isValidPosition(row, col)) {
+            // tilføjer potentielle naboer inkl. diagonale
+            const potentialNeighbours = [
+                { row: row - 1, col: col - 1 }, // nord-vest
+                { row: row - 1, col }, // nord
+                { row: row - 1, col: col + 1 }, // nord-øst
+                { row, col: col - 1 }, // vest
+                { row, col: col + 1 }, // øst
+                { row: row + 1, col }, // syd
+                { row: row + 1, col: col - 1 }, // syd-vest
+                { row: row + 1, col: col + 1 }, // syd-øst
+            ];
+
+            // filtrere de nabo positioner der er uden for griddet
+            neighboursActual = potentialNeighbours.filter((pos) => this._isValidPosition(pos.row, pos.col));
+
+            return neighboursActual;
+        } else {
+            return undefined;
+        }
+    }
+
+    neighbourValues(arg1, arg2) {
+        const neighbours = this.neighbours(arg1, arg2);
+        const neighbourValues = neighbours.map((pos) => this.get(pos.row, pos.col));
+        return neighbourValues;
+    }
+
+    nextInRow(arg1, arg2) {
+        const { row, col } = this._parsePosition(arg1, arg2);
+        const value = this.get(row, col + 1);
+
+        if (this._isValidPosition(row, col)) {
+            return { row, col: col + 1, value };
+        } else {
+            return undefined;
+        }
+    }
+
+    nextInCol(arg1, arg2) {
+        const { row, col } = this._parsePosition(arg1, arg2);
+        const value = this.get(row + 1, col);
+
+        if (this._isValidPosition(row + 1, col)) {
+            return { row: row + 1, col, value };
+        } else {
+            return undefined;
+        }
+    }
+
+    north(arg1, arg2) {
+        const { row, col } = this._parsePosition(arg1 - 1, arg2);
+        const value = this.get(row, col);
+
+        if (this._isValidPosition(row, col)) {
+            return { row, col, value };
+        } else {
+            return undefined;
+        }
+    }
+
+    south(arg1, arg2) {
+        const { row, col } = this._parsePosition(arg1 + 1, arg2);
+        const value = this.get(row, col);
+
+        if (this._isValidPosition(row, col)) {
+            return { row, col, value };
+        } else {
+            return undefined;
+        }
+    }
+
+    west(arg1, arg2) {
+        const { row, col } = this._parsePosition(arg1, arg2 - 1);
+        const value = this.get(row, col);
+
+        if (this._isValidPosition(row, col)) {
+            return { row, col, value };
+        } else {
+            return undefined;
+        }
+    }
+
+        east(arg1, arg2) {
+        const { row, col } = this._parsePosition(arg1, arg2 + 1);
+        const value = this.get(row, col);
+
+        if (this._isValidPosition(row, col)) {
+            return { row, col, value };
         } else {
             return undefined;
         }
--- grid/test_grid.js
+++ grid/test_grid.js
@@ -5,6 +5,8 @@ const grid = new Grid(3, 3); // grid med cellers index value !!! --- passer til 
 // const grid = new Grid(10, 10); // grid med cellers index value
 
 // test set(row, col)
+console.log("\n ----------- testing set -----------");
+
 grid.set(1, 2, "Fem");
 const setTest1 = grid.get(1, 2);
 console.log(`grid value at (1,2) set to: ${setTest1}`); // expected: Fem
@@ -22,18 +24,73 @@ const setTest4 = grid.get({ row: 300, col: 200 });
 console.log(`grid value at (300, 100) set to: ${setTest4}`); // expected: undefined
 
 // test get(row, col)
-const gridValue = grid.get(2, 2);
+console.log("\n ----------- testing get -----------");
+const gridValue = grid.get(1, 2);
 console.log(`gridValue is ${gridValue}`); // expected: 8
 
 // test indexfor()
+console.log("\n ----------- testing indexfor -----------");
 const index = grid.indexFor(2, 1);
 console.log(`Index is ${index}`); // expected: 7
 
 // test rowColFor(index)
+console.log("\n ----------- testing rowColFor -----------");
 const gridCoordinate = grid.rowColFor(3);
 console.log(`Grid coordinate at index: ${JSON.stringify(gridCoordinate)}`); // expected: {row: 1, col: 0}
 const gridCoordinateInvalid = grid.rowColFor(8000);
 console.log(`Grid coordinate at index: ${JSON.stringify(gridCoordinateInvalid)}`); // expected: undefined
+
+// test neighbours
+console.log("\n ----------- testing neighbours -----------");
+const neighbours = grid.neighbours(2, 2);
+console.log(neighbours);
+
+// test neighbourValues( row, col )
+console.log("\n ----------- testing neighbourValues -----------");
+const neighboursValues = grid.neighbourValues(1, 1);
+console.log(neighboursValues);
+
+// test nextInRow( row, col )
+console.log("\n ----------- testing nextInRow -----------");
+const nextCellInRow = grid.nextInRow(0, 0);
+console.log(nextCellInRow); // expected {0, 1, 'Et'}
+// const nextCellInRow = grid.nextInRow(20, 22)
+// console.log(nextCellInRow) // expected undefined
+
+// test nextInCol(arg1, arg2)
+console.log("\n ----------- testing nextInCol -----------");
+const nextCellInCol = grid.nextInCol(0, 2);
+console.log(nextCellInCol); // expected {1, 2, 'Fem'}
+// const nextCellInCol = grid.nextInCol(30, 32)
+// console.log(nextCellInCol); // expected undefined
+
+console.log("\n ----------- testing north -----------");
+// test north(row, col)
+const north = grid.north(1, 1);
+console.log(north); // expected {0, 1, 'Et'}
+// const north = grid.north(0, 1);
+// console.log(north); // expected undefined
+
+console.log("\n ----------- testing south -----------");
+// test south(row, col)
+const south = grid.south(1, 1);
+console.log(south); // expected {2, 1, 7}
+// const south = grid.south(2, 1);
+// console.log(south); // expected undefined
+
+console.log("\n ----------- testing west -----------");
+// test west(row, col)
+const west = grid.west(1, 1);
+console.log(west); // expected {1, 0, 3}
+// const west = grid.west(0, 0);
+// console.log(west); // expected undefined
+
+console.log("\n ----------- testing east -----------");
+// test east(row, col)
+const east = grid.east(1, 1);
+console.log(east); // expected {1, 2, 'Fem'}
+// const east = grid.east(0, 2);
+// console.log(east); // expected undefined
 
 // dump
 grid.dump();
