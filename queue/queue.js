65738bc34ba8773ea2b40fa69329d9445abb13f0
Jon <lundbyjon@hotmail.com>
2024-10-01 17:53:18 +0200

    oprydning i kommentarer

--- queue/queue.js
+++ queue/queue.js
@@ -1,5 +1,4 @@
 class Node {
-    // next = null, hvis der ikke er nogen næste node
     constructor(data, next = null) {
         this.data = data;
         this.next = next;
@@ -27,7 +26,6 @@ export default class Queue {
         }
     }
 
-    // tilføjer en ny node, med reference til data-objektet, bagest i køen
     enqueue(data) {
         const newNode = new Node(data)
 
@@ -40,7 +38,6 @@ export default class Queue {
         }
     }
 
-    // fjerner den node der ligger forrest i køen, og returnerer det referede data-objekt
     dequeue() {
         if (this.head === null) {
             return
@@ -58,12 +55,10 @@ export default class Queue {
         return nodeToRemove;
     }
     
-    // returnerer data-objektet der ligger forrest i køen, uden at fjerne det
     peek() {
         return this.head ? this.head.data : null;
     }
 
-    // fortæller hvor mange elementer der er i køen
     size() {
         let current = this.head;
         let count = 0;
@@ -76,7 +71,6 @@ export default class Queue {
         return count;
    }
 
-    // finder og returnerer elementet på plads ‘index’, hvor 0 er det forreste, uden at fjerne noget
     get(index) {
         if (typeof index === "number" && index < this.size() && index >= 0) {
             let current = this.head;
