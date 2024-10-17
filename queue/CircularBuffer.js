96774cc354bad4ec6a53a74b0032710c7bb0d1c0
Jon <lundbyjon@hotmail.com>
2024-10-17 16:44:57 +0200

    all circularBuffer methods implemented, not fully tested or complete

--- queue/CircularBuffer.js
+++ queue/CircularBuffer.js
@@ -22,16 +22,67 @@ export default class CircularBuffer {
     }
 
     enqueue(data) {
-        this.array[this.writeIndex] = data;
-        this.writeIndex++;
+        // checking if the write pointer is still less than the static array size and if it is then there must still be empty indexes
+        if (this.writeIndex < this.arraySize) {
+            this.array.set(this.writeIndex, data);
+            this.writeIndex++;
+        } else {
+            // else the write pointer must be pointing at the last index which would already have been set. Thus resetting the write pointer to 0 and overwriting from index 0
+            this.writeIndex = 0;
+            this.array.set(this.writeIndex, data);
+        }
     }
 
     dequeue() {
-        this.array[this.readIndex] = null;
-        this.readIndex++;
+        if (this.readIndex < this.arraySize) {
+            this.array.set(this.readIndex, null);
+            this.readIndex++;
+        } else {
+            this.readIndex = 0;
+            this.array.set(this.readIndex, null);
+            this.readIndex++;
+        }
+    }
+
+    peek() {
+        return this.array.get(this.readIndex);
+    }
+
+    size() {
+        let count = 0;
+
+        for (const element of this.array) {
+            if (element !== null && element !== undefined) {
+                count++;
+            }
+        }
+        return count;
     }
 
     get(index) {
+        if (index < 0 || index > this.arraySize) {
+            throw new Error("Invalid index. Out of bounds!");
+        }
         return this.array.get(index);
+    }
+
+    isEmpty() {
+        let size = this.size()
+        if (size === 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    isFull() {
+        let size = this.size()
+        if (size === this.arraySize) {
+            return true;
+        }
+        return false;
+    }
+
+    capacity() {
+        return this.arraySize
     }
 }
--- queue/test_circularBuffer.js
+++ queue/test_circularBuffer.js
@@ -2,16 +2,71 @@ import CircularBuffer from "./CircularBuffer.js";
 
 const buffer = new CircularBuffer();
 
-buffer.enqueue("F")
-buffer.enqueue("O")
-buffer.enqueue("U")
-buffer.enqueue("R")
+console.log("Empty buffer: ", buffer.isEmpty()); // expected: true
+console.log("Full buffer: ", buffer.isFull()); // expected: false
+console.log("Buffer elements: ", buffer.size()); // expected: 0
+console.log("capacity: ", buffer.capacity()); // 4
 
-// const test = buffer.get(1)
-// console.log(test)
+
+buffer.enqueue("F");
+buffer.enqueue("O");
+buffer.enqueue("U");
+buffer.enqueue("R");
 
 for (const element of buffer) {
-    console.log(element)
-}
+    console.log(element);
+} // expected output: F O U R
 
-console.log("----- TEST DONE -----")
+console.log("Empty buffer: ", buffer.isEmpty()); // expected: false
+console.log("Full buffer: ", buffer.isFull()); // expected: true
+console.log("Buffer elements: ", buffer.size()); // expected: 4
+console.log("capacity: ", buffer.capacity()); // 4
+
+
+console.log("----- TESTED: isEmpty, isFull, size, capacity, enqueue, iterator -----");
+
+// buffer.dequeue();
+// buffer.dequeue();
+
+// for (const element of buffer) {
+//     console.log(element);
+// } // expected output: null null U R
+
+// console.log("----- TESTED: null null U R -----");
+
+// buffer.enqueue("Q");
+
+// for (const element of buffer) {
+//     console.log(element);
+// } // expected output: Q null U R
+
+// console.log("----- TESTED: Q null U R -----");
+
+// buffer.dequeue();
+// buffer.dequeue();
+// buffer.dequeue();
+
+// for (const element of buffer) {
+//     console.log(element);
+// } // expected output: null null null null
+
+// console.log("----- TESTED: null null null null -----");
+
+// ---------------------------------------------------------- \\
+
+// buffer.enqueue("F");
+// buffer.enqueue("O");
+// buffer.enqueue("U");
+// buffer.enqueue("R");
+
+// buffer.enqueue("Q")
+// buffer.dequeue()
+
+// for (const element of buffer) {
+//     console.log(element);
+// } // expected output: Q null U R
+
+// console.log("----- TESTED: Q null U R -----");
+
+
+// console.log("\n----- TEST DONE -----");
