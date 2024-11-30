87627a530d8f26e391ad7cbd99871335fee24791
Jon <lundbyjon@hotmail.com>
2024-12-01 00:33:36 +0100

    all tree class methods implemented

--- tree/test_tree.js
+++ tree/test_tree.js
@@ -1,37 +1,80 @@
 import Node from "./tree.js";
+import { Tree } from "./tree.js";
 
-// test instantiering a ny node uden children
-let myNode = new Node("node1");
-console.log(myNode);
+// // test instantiering af ny node uden children
+// let myNode = new Node("node1");
+// console.log(myNode);
 
-// tjek first child     expected: null
-console.log("first child null?: ", myNode.firstChild());
+// // tjek first child
+// console.log("first child null?: ", myNode.firstChild()); // expected: null;
 
-// tjek last child     expected: null
-console.log("last child null?: ", myNode.lastChild());
+// // tjek last child
+// console.log("last child null?: ", myNode.lastChild()); // expected: null
 
-// tjek has child nodes     expected: false
-console.log("has child false?: ", myNode.hasChildNodes());
+// // tjek has child nodes
+// console.log("has child false?: ", myNode.hasChildNodes()); // expected: false
 
-// tjek append child
-let newNode2 = new Node("node2");
-myNode.appendChild(newNode2);
+// // tjek append child
+// let newNode2 = new Node("node2");
+// myNode.appendChild(newNode2);
 
-let newNode3 = new Node("node3");
-myNode.appendChild(newNode3);
+// let newNode3 = new Node("node3");
+// myNode.appendChild(newNode3);
 
-let newNode4 = new Node("node4");
-myNode.appendChild(newNode4);
+// let newNode4 = new Node("node4");
+// myNode.appendChild(newNode4);
 
-// test om myNode har children og print af myNode   expected: true og Node1 med Node2-3-4 i childnodes array
-console.log("has child null?: ", myNode.hasChildNodes());
-console.log("Node with 3 children: ", myNode);
+// // test om myNode har children og print af myNode   expected: true og Node1 med Node2-3-4 i childnodes array
+// console.log("has child true?: ", myNode.hasChildNodes()); // expected: true
+// console.log("Node with 3 children: ", myNode);
 
-// test removChild newNode3     expected: Node1 med Node2 og Node4 som childnodes
-myNode.removeChild(newNode3);
-console.log("Node with 2 children (Node2 & Node4): ", myNode);
+// // test removChild newNode3     expected: Node1 med Node2 og Node4 som childnodes
+// myNode.removeChild(newNode3);
+// console.log("Node with 2 children (Node2 & Node4): ", myNode);
 
-// test udskifting af nodes     expected: Node1 med Node5 i stedet for Node4.
-let newNode5 = new Node("node5");
-myNode.replaceChild(newNode5, newNode4);
-console.log(myNode);
+// // test udskifting af nodes
+// let newNode5 = new Node("node5");
+// myNode.replaceChild(newNode5, newNode4);
+// console.log(myNode); // expected: Node1 med Node5 i stedet for Node4.
+
+let myTree = new Tree();
+console.log("Empty tree: ", myTree);
+console.log("------------ tree dump ------------");
+myTree.dump();
+
+myTree.addValue("Tree node 1");
+console.log("Tree with only root: ");
+myTree.dump();
+
+myTree.addValue("Tree node 2");
+console.log("Tree with root and one child: ");
+myTree.dump();
+
+myTree.addValue("Tree node 3");
+myTree.addValue("Tree node 4");
+myTree.addValue("Tree node 5");
+myTree.addValue("Tree node 6");
+myTree.addValue("Tree node 7");
+myTree.addValue("Tree node 8");
+myTree.addValue("Tree node 9");
+myTree.addValue("Tree node 10");
+console.log("------------ new tree dump ------------");
+myTree.dump();
+
+// test af ikke eksisterende værdi
+const foundNonExistingValue = myTree.findValue("Tree node 100");
+console.log("Found value: ", foundNonExistingValue); // expected: "No node matches the value" / null
+
+// test af tomt træ
+const myEmptyTree = new Tree();
+const foundValueInEmptyTree = myEmptyTree.findValue("True node 100.¤¤");
+console.log("Found value: ", foundValueInEmptyTree); // expected: "Tree is empty" / null
+
+// test af eksisterende værdi i træet
+const foundValue = myTree.findValue("Tree node 10");
+console.log("Found value: ", foundValue); // expected: node with value "Tree node 10"
+
+myTree.addValue("Tree node 11");
+console.log("------------ new tree dump (removed node(3) should and children(10, 11) should be missing)------------");
+myTree.removeValue("Tree node 3");
+myTree.dump();
--- tree/tree.js
+++ tree/tree.js
@@ -54,24 +54,99 @@ export class Tree {
         this.root = node || null;
     }
 
-    // - `addValue( value )` - der opretter en ny node med den givne value, og tilføjer den et sted i træet - du bestemmer selv hvor!
     addValue(value) {
         const newNode = new Node(value);
 
+        // i tilfælde af et tomt træ sætte den nye node som root
         if (this.root === null) {
             this.root = newNode;
         } else {
-            //
-            // this.root.hasChildNodes
+            const queue = [this.root]; // en kø initialiseres fra roden af træet
+
+            while (queue.length > 0) {
+                let currentParent = queue[0]; // første element i køen er currentParent
+                queue.shift(); // første element fra køen fjernes
+
+                // hvis currentParent har mindre en 4 children så kan den ny node tilføjes
+                // altså bliver der her sat en begrænsning på hvor mange children en parentNode kan have, her 3
+                if (currentParent.childNodes.length < 4) {
+                    currentParent.appendChild(newNode);
+                    return;
+                }
+
+                // pusher resten af currentParents children til køen
+                queue.push(...currentParent.childNodes);
+            }
         }
     }
 
-    // - `findValue( value )` - der leder efter den givne value i træet, og returnerer den (første) Node der har den
-    findValue(value) {}
+    findValue(value) {
+        if (!this.root) {
+            console.log("Tree is empty");
+            return null;
+        } else {
+            const queue = [this.root];
 
-    // - `removeValue( value )` - der finder og fjerner Noden med den givne value fra træet
-    removeValue() {}
+            while (queue.length > 0) {
+                let currentNode = queue.shift();
 
-    //     - `dump()` - der udskriver hele træet i consollen.
-    dump() {}
+                if (currentNode.value === value) {
+                    return currentNode; // returnerer den første node der matcher value
+                }
+
+                queue.push(...currentNode.childNodes);
+            }
+
+            console.log("No node matches the value");
+            return null;
+        }
+    }
+
+    removeValue(value) {
+        // finder noden der skal slettes
+        const nodeToRemove = this.findValue(value);
+
+        // tjekker om noden eksisterer
+        if (!nodeToRemove) {
+            throw new Error("Could not find value to remove");
+        }
+
+        // tjekker om noden der skal fjernes er root noden
+        if (nodeToRemove === this.root) {
+            this.root = null;
+            console.log("root node removed");
+
+            return; // hvis dette er tilfældet return tidligt
+        }
+
+        // efter de forrige tjek kan parentnoden til den node der skal fjernes findes
+        const parentNode = nodeToRemove.parentNode;
+
+        // ekstra tjek om parent noden er falsy
+        if (!parentNode) {
+            throw new Error("Node has no parent, tree structure might be invalid");
+        }
+
+        // hvis parentNode ikke er falsy så kan den bruges som node til at kalde removeChild med den value der skal fjernes
+        console.log("node removed");
+        parentNode.removeChild(nodeToRemove);
+    }
+
+    dump() {
+        function printNode(node, depth) {
+            // for at skabe en træ-agtig struktur i dump printet så repeates "--" "depth" antal gange og konkatineres med nodens value
+            console.log("depth -" + "--".repeat(depth) + "> " + node.value);
+
+            // hvert child af noden fra det oprindelige printNode kald bliver brugt som argument i et rekursivt kald af printNode
+            for (let child of node.childNodes) {
+                printNode(child, depth + 1);
+            }
+        }
+
+        if (this.root === null) {
+            console.log("The tree is empty");
+        } else {
+            printNode(this.root, 0);
+        }
+    }
 }
